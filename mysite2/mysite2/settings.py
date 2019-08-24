2467c8d9abda55aef6c7c4cfd13dc295d64d4720
Luke M <luke.moran2004@gmail.com>
2019-08-24 10:44:53 +0100

    Fixed CSS loading issues

--- mysite2/mysite2/settings.py
+++ mysite2/mysite2/settings.py
@@ -117,10 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-
+STATIC_BLOG = "blog/static/css"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, STATIC_BLOG),
+]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+print(STATIC_ROOT, "Static root")
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
